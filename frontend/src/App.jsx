a3c4f77441ee559535e2f2aa7cee9f97bb0b5e4f
joshgore7 <josh.gore77@gmail.com>
2025-04-01 16:32:49 -0400

    weather app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Weather from './Weather'
+
+function App() {
+
+
+  return (
+    <>
+      <Weather />
+      <h1>Weather App</h1>
+    </>
+  )
+}
+
+export default App
